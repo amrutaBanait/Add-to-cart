a2c998f3743f3d51326f2c6f30f79efcfda7cbdc
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-02-22 16:01:37 +0530

    header component added

---
+++ shopping_cart/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../App.css"
+
+const Header = () => {
+    return (
+        <div className="flex shopping-card">
+          <div>Shopping Cart</div>
+          <div>Cart
+            <sup>{"2"}</sup>
+          </div>
+          </div>
+      )
+}
+
+export default Header
