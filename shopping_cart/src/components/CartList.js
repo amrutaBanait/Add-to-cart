dd0fc4e9f1e9fe2edb93b119ab3b7ee6efbd970c
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-02-25 21:19:32 +0530

    total price updated

--- shopping_cart/src/components/CartList.js
+++ shopping_cart/src/components/CartList.js
@@ -40,7 +40,7 @@ function CartList({ cart }) {
       }
       <p>Total: <span></span>
         {
-            cart.map(item => item.price * item.quantity).reduce((total, value) => total + value, 0)
+            CART.map(item => item.price * item.quantity).reduce((total, value) => total + value, 0)
         }
       </p>
     </div>
