c8f176b476a614ac6a86245767b2de0ba3217163
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-02-27 15:24:26 +0530

    styling added

--- shopping_cart/src/components/CartList.js
+++ shopping_cart/src/components/CartList.js
@@ -2,46 +2,59 @@ import { useState, useEffect } from "react";
 import "../App.css";
 
 function CartList({ cart }) {
-    const [CART, setCART] = useState([])
+  const [CART, setCART] = useState([]);
 
-    useEffect(()=>{
-        setCART(cart)
-    }, [cart])
+  useEffect(() => {
+    setCART(cart);
+  }, [cart]);
 
   return (
     <div>
-      {
-     CART?.map((cartItem, cartindex) => {
+      {CART?.map((cartItem, cartindex) => {
         return (
           <div>
-            <img src={cartItem.url} width={40} />
+            <img src={cartItem.url} width={70} />
             <span>{cartItem.name} </span>
             <button
-            onClick ={ () =>{
-              const _CART = CART.map((item, index) =>{
-                return cartindex === index ? { ...item, quantity : item.quantity - 1} : item
-              })
-              setCART (_CART)
-            }}
-            >-</button>
+             className="add btn"
+              onClick={() => {
+                const _CART = CART.map((item, index) => {
+                  return cartindex === index
+                    ? {
+                        ...item,
+                        quantity: item.quantity > 0 ? item.quantity - 1 : 0,
+                      }
+                    : item;
+                });
+                setCART(_CART);
+              }}
+            >
+              -
+            </button>
             <span>{cartItem.quantity}</span>
             <button
-                                onClick={() => {
-                                    const _CART = CART.map((item, index) => {
-                                        return cartindex === index ? { ...item, quantity: item.quantity + 1 } : item
-                                    })
-                                    setCART(_CART)
-                                }}
-                            >+</button>
+              className="add btn"
+              onClick={() => {
+                const _CART = CART.map((item, index) => {
+                  return cartindex === index
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item;
+                });
+                setCART(_CART);
+              }}
+            >
+              +
+            </button>
             <span>Rs. {cartItem.price * cartItem.quantity}</span>
           </div>
         );
-      })
-      }
-      <p>Total: <span></span>
-        {
-            CART.map(item => item.price * item.quantity).reduce((total, value) => total + value, 0)
-        }
+      })}
+      <p>
+        Total: <span></span>
+        {CART.map((item) => item.price * item.quantity).reduce(
+          (total, value) => total + value,
+          0
+        )}
       </p>
     </div>
   );
--- shopping_cart/src/components/ProductList.js
+++ shopping_cart/src/components/ProductList.js
@@ -8,13 +8,14 @@ function ProductList({product, addToCart}){
        {
         product.map((productItem, productIndex ) =>{
             return (
-                <div style={{width: "50%" }}>
+                <div className="product" style={{width: "30%"}}>
                     <div className="product-item">
                         <img src = {productItem.url} width="100%"/>
                         <p> {productItem.name} | {productItem.category} </p>
                         <p> {productItem.seller} </p>
                         <p> Rs. {productItem.price} /- </p>
                         <button 
+                        className='btn'
                         onClick ={()=> addToCart(productItem)}
                         >Add To Cart</button>
                     </div>
